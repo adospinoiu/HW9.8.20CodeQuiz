543d7c6b48b47075885fde2f6c19c2a459f295aa
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-08-31 22:04:15 -0700

    Added the seven questions that will be on the quiz. And seven answers

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ button1.textContent = "1. Hard To Master Language";
 button2.textContent = "2. Hat To Mister Lancaster";
 button3.textContent = "3. Have To Many Laughs";
 button4.textContent = "4. Hypertext Markup Language";
+
+//Question #2
+question.textContent = "What does CSS stand for?";
+button1.textContent = "1. Clear Sign Security";
+button2.textContent = "2. Cascading Style Sheets";
+button3.textContent = "3. Clean Stop Signs";
+button4.textContent = "4. Come Save Sam";
+
+//Question #3
+question.textContent = "Which of the following is FALSE?";
+button1.textContent = "1. 1 === 1";
+button2.textContent = "2. TRUE === TRUE";
+button3.textContent = "3. 5 < 6";
+button4.textContent = "4. 5 != 5";
+
+//Question #4
+question.textContent = "What is Bootstrap?";
+button1.textContent = "1. Famous Bootstrap Bill";
+button2.textContent = "2. You use it to tie your boots";
+button3.textContent = "3. CSS on steroids that can be used";
+button4.textContent = "4. There's a snake in my boot";
+
+//Question #5
+question.textContent = "An array is surrounded by?";
+button1.textContent = "1. Single quotation marks";
+button2.textContent = "2. Double quotation marks";
+button3.textContent = "3. Curly brackets";
+button4.textContent = "4. Scare brackets";
+
+//Question #6
+question.textContent = "What is the first line when pushing to GitHub?";
+button1.textContent = "1. git commit -m";
+button2.textContent = "2. git push";
+button3.textContent = "3. git add .";
+button4.textContent = "4. What the heck is GitHub";
+
+//Question #7
+question.textContent = "When is an elseif statement executed?";
+button1.textContent = "1. When the else is not there";
+button2.textContent = "2. When the if is not there";
+button3.textContent = "3. If it feels like it";
+button4.textContent = "4. When the if is false";
