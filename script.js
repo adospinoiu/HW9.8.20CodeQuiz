70ab2105e5c6a09d48cb618f474641278246e3ff
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-08-31 21:21:00 -0700

    Started working on the script.js file. Set all the top level const

---
+++ script.js
@@ -0,0 +1,12 @@
+const question = document.querySelector("#question");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+
+//Question #1
+question.textContent = "What does HTML stand for?";
+button1.textContent = "1. Hard To Master Language";
+button2.textContent = "2. Hat To Mister Lancaster";
+button3.textContent = "3. Have To Many Laughs";
+button4.textContent = "4. Hypertext Markup Language";
