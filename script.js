1b12fdcbd9ead5368927559bbbeceeae5160a228
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-01 22:07:04 -0700

    Fixed the buttons so they alert and compare to correct answer

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const timeEl = document.querySelector(".time");
 let secondsLeft = 60;
 
 function setTime() {
-  let timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time left: " + secondsLeft;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
       //sendMessage();
     }
@@ -25,75 +25,130 @@ setTime();
 
 let questionAnswer = [
   {
-    Question: "What does HTML stand for?",
-    Answers: ["Hard To Master Language",
+    question: "What does HTML stand for?",
+    answers: ["Hard To Master Language",
       "Hat To Mister Lancaster",
       "Have To Many Laughs",
       "Hypertext Markup Language"
     ],
-    CorrectAnswer: "Hypertext Markup Language"
+    correctAnswer: "Hypertext Markup Language"
   },
 
   {
-    Question: "What does CSS stand for?",
-    Answers: ["Clear Sign Security",
+    question: "What does CSS stand for?",
+    answers: ["Clear Sign Security",
       "Cascading Style Sheets",
       "Clean Stop Signs",
       "Come Save Sam"
     ],
-    CorrectAnswer: "Cascading Style Sheets"
+    correctAnswer: "Cascading Style Sheets"
   },
 
   {
-    Question: "Which of the following is FALSE?",
-    Answers: ["1 === 1",
+    question: "Which of the following is FALSE?",
+    answers: ["1 === 1",
       "TRUE === TRUE",
       "5 < 6",
       "5 != 5"
     ],
-    CorrectAnswer: "5 != 5"
+    correctAnswer: "5 != 5"
   },
 
   {
-    Question: "What is Bootstrap?",
-    Answers: ["Famous Bootstrap Bill",
+    question: "What is Bootstrap?",
+    answers: ["Famous Bootstrap Bill",
       "You use it to tie your boots",
       "CSS on steroids that can be used",
       "There's a snake in my boot"
     ],
-    CorrectAnswer: "CSS on steroids that can be used"
+    correctAnswer: "CSS on steroids that can be used"
   },
 
   {
-    Question: "An array is surrounded by?",
-    Answers: ["Single quotation marks",
+    question: "An array is surrounded by?",
+    answers: ["Single quotation marks",
       "Double quotation marks",
       "Curly brackets",
       "Square brackets"
     ],
-    CorrectAnswer: "Square brackets"
+    correctAnswer: "Square brackets"
   },
 
   {
-    Question: "What is the first line when pushing to GitHub?",
-    Answers: ["git commit -m",
+    question: "What is the first line when pushing to GitHub?",
+    answers: ["git commit -m",
       "git push",
       "git add .",
       "What the heck is GitHub"
     ],
-    CorrectAnswer: "git add ."
+    correctAnswer: "git add ."
   },
 
   {
-    Question: "When is an elseif statement executed?",
-    Answers: ["When the else is not there",
+    question: "When is an elseif statement executed?",
+    answers: ["When the else is not there",
       "When the if is not there",
       "If it feels like it",
       "When the if is false"
     ],
-    CorrectAnswer: "When the if is false"
+    correctAnswer: "When the if is false"
   },
 ]
 
+let index = 0;
+let currentQuestion = questionAnswer[0]
 
+nextQuestion();
 
+function nextQuestion() {
+  currentQuestion = questionAnswer[index]
+  question.textContent = currentQuestion.question;
+  button1.textContent = currentQuestion.answers[0];
+  button2.textContent = currentQuestion.answers[1];
+  button3.textContent = currentQuestion.answers[2];
+  button4.textContent = currentQuestion.answers[3];
+}
+
+button1.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (button1.textContent === currentQuestion.correctAnswer) {
+    alert("correct");
+  } else {
+    alert("wrong");
+  }
+  index++;
+  nextQuestion();
+});
+
+button2.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (button2.textContent === currentQuestion.correctAnswer) {
+    alert("correct");
+  } else {
+    alert("wrong");
+  }
+  index++;
+  nextQuestion();
+});
+
+button3.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (button3.textContent === currentQuestion.correctAnswer) {
+    alert("correct");
+  } else {
+    alert("wrong");
+  }
+  index++;
+  nextQuestion();
+});
+
+button4.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (button4.textContent === currentQuestion.correctAnswer) {
+    alert("correct");
+  } else {
+    alert("wrong");
+  }
+  index++;
+  nextQuestion();
+});
