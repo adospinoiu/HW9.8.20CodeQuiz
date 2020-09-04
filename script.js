6155b941c336e9ef37d12732028fedb9aa947ca8
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-03 18:33:17 -0700

    Added form to log the score. Also made the score show up at the end

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+const body = document.body;
+const h4El = document.createElement("h4");
+
 const question = document.querySelector("#question");
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const button4 = document.querySelector("#button4");
 const timeEl = document.querySelector(".time");
+const buttons = document.querySelector("#buttons");
+const logInitials = document.querySelector("#logInitials");
 
-//Timer in the upper right hand corner
-let secondsLeft = 60;
 
-function setTime() {
+// Timer in the upper right hand corner
+let secondsLeft = 90;
+
+function setTime(end) {
   let timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = "Time left: " + secondsLeft;
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      //sendMessage();
-    }
+        }
 
   }, 1000);
+
+  if (end) {
+    clearInterval(timerInterval);
+    setTime(true);
+  }
 }
 
+// Call the Timer to start running
 setTime();
 
+// All the questions and answers in a large array of objects
 let questionAnswer = [
   {
     question: "What does HTML stand for?",
@@ -100,55 +112,74 @@ let currentQuestion = questionAnswer[0]
 
 nextQuestion();
 
+// Calls the questions and all the answers associated with the question
 function nextQuestion() {
-  currentQuestion = questionAnswer[index]
-  question.textContent = currentQuestion.question;
-  button1.textContent = currentQuestion.answers[0];
-  button2.textContent = currentQuestion.answers[1];
-  button3.textContent = currentQuestion.answers[2];
-  button4.textContent = currentQuestion.answers[3];
+  if (index != questionAnswer.length) {
+    currentQuestion = questionAnswer[index]
+    question.textContent = currentQuestion.question;
+    button1.textContent = currentQuestion.answers[0];
+    button2.textContent = currentQuestion.answers[1];
+    button3.textContent = currentQuestion.answers[2];
+    button4.textContent = currentQuestion.answers[3];
+  } else {
+    question.textContent = "Your Score Is: " + secondsLeft;
+    buttons.setAttribute("style", "display:none");
+    logInitials.setAttribute("style", "display:block");
+
+  }
 }
 
+
+//Evaluates the user button clicked and determines if it is the correct answer. 
 button1.addEventListener("click", function (event) {
   event.preventDefault();
   if (button1.textContent === currentQuestion.correctAnswer) {
     alert("correct");
   } else {
     alert("wrong");
+    secondsLeft = secondsLeft - 10;
   }
   index++;
   nextQuestion();
 });
 
+//Evaluates the user button clicked and determines if it is the correct answer. 
 button2.addEventListener("click", function (event) {
   event.preventDefault();
   if (button2.textContent === currentQuestion.correctAnswer) {
     alert("correct");
   } else {
     alert("wrong");
+    secondsLeft = secondsLeft - 10;
   }
   index++;
   nextQuestion();
 });
 
+//Evaluates the user button clicked and determines if it is the correct answer. 
 button3.addEventListener("click", function (event) {
   event.preventDefault();
   if (button3.textContent === currentQuestion.correctAnswer) {
     alert("correct");
   } else {
     alert("wrong");
+    secondsLeft = secondsLeft - 10;
   }
   index++;
   nextQuestion();
 });
 
+//Evaluates the user button clicked and determines if it is the correct answer. 
 button4.addEventListener("click", function (event) {
   event.preventDefault();
   if (button4.textContent === currentQuestion.correctAnswer) {
     alert("correct");
   } else {
     alert("wrong");
+    secondsLeft = secondsLeft - 10;
   }
   index++;
   nextQuestion();
 });
+
+
